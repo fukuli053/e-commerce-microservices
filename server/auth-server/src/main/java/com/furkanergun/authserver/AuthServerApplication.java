81c01e85efce88dd0483abc4767f660c9ac420f2
Furkan Ergün <furkan.ergun@ogr.sakarya.edu.tr>
2021-12-14 13:21:26 +0300

    Auth Server Eureka Client

--- server/auth-server/src/main/java/com/furkanergun/authserver/AuthServerApplication.java
+++ server/auth-server/src/main/java/com/furkanergun/authserver/AuthServerApplication.java
@@ -3,8 +3,10 @@ package com.furkanergun.authserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AuthServerApplication {
 
 	public static void main(String[] args) {
