6bd40a9f1ba11a936b6567989645e6fd95279268
Furkan Ergün <furkan.ergun@ogr.sakarya.edu.tr>
2021-12-14 13:51:26 +0300

    Removed user service

--- server/user-service/src/test/java/com/furkanergun/userservice/UserServiceApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.furkanergun.userservice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class UserServiceApplicationTests {
-
-}
