75e01b983d09061e8aec8261e710fd6c3652700a
fukuli053 <fergun3453@hotmail.com>
2021-12-31 01:27:47 +0300

    redis problem fixed

--- server/gateway-service/src/main/java/com/furkanergun/gatewayservice/config/GatewayConfig.java
+++ server/gateway-service/src/main/java/com/furkanergun/gatewayservice/config/GatewayConfig.java
@@ -18,8 +18,12 @@ public class GatewayConfig {
         return builder.routes().route("auth", r -> r.path("/auth/**")
                 .filters(f -> f.filter(filter).rewritePath("/auth/(?<segment>.*)", "/$\\{segment}"))
                 .uri("lb://AUTH-SERVER"))
-                .route("alert", r -> r.path("/alert/**").filters(f -> f.filter(filter)).uri("lb://alert"))
+                .route("products", r -> r.path("/products/**")
+                        .filters(f -> f.filter(filter).rewritePath("/products/(?<segment>.*)", "/$\\{segment}"))
+                        .uri("lb://PRODUCT-SERVICE"))
+                .route("suggestion-service", r -> r.path("/suggestion/**")
+                        .filters(f -> f.filter(filter).rewritePath("/suggestion/(?<segment>.*)", "/$\\{segment}"))
+                        .uri("lb://SUGGESTION-SEARCH-SERVICE"))
                 .build();
     }
-
 }
